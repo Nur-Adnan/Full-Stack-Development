3246c0436dbe95a88a715562f6d593266be41459
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-20 00:03:48 +0600

    length string

---
+++ 1 - Basic API/1 - String.js
@@ -0,0 +1,11 @@
+// String handbook
+
+// String: length, indexOf(), lastIndexOf(), slice(), substring(), replace()
+// split(), trim(), toUpperCase(), toLowerCase(), etc.
+
+function getLength(str) {
+  console.log("Original String: ", str);
+  console.log("Length: ", str.length);
+}
+
+getLength("Hello World");
