3f054038e358e2c29286396154e1cd7404e723f2
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-20 23:38:30 +0600

    pop , push, shift, unshift

---
+++ 1 - Basic API/3 - Arrays.js
@@ -0,0 +1,26 @@
+// Array handbook
+
+// Array: push(), pop(), shift(), unshift(), splice(), slice(),
+// concat(), forEach(), map(), filter(), reduce(), find(), sort()
+
+//push
+const initArray1 = [1, 2, 4];
+initArray1.push(3);
+console.log(initArray1);
+
+//pop
+const initArray2 = [1, 2, 4];
+initArray2.pop();
+console.log(initArray2);
+
+// shift
+
+const initArray3 = [1, 2, 3, 4];
+initArray3.shift();
+console.log(initArray3);
+
+//unshift
+
+const initArray4 = [2, 3, 4, 5];
+initArray4.unshift(1);
+console.log(initArray4);
