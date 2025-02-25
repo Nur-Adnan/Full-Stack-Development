ba5b011aabd6cd3b39dc055badf0021352e8e533
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-26 00:55:08 +0600

    understand async and await

---
+++ 2 - async, await and promise/async_await.js
@@ -0,0 +1,20 @@
+function adnanAsyncFunction() {
+  let p = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("Hi There !");
+    }, 3000);
+  });
+  return p;
+}
+
+async function main() {
+  // no callbacks, no .then syntax
+  const value = adnanAsyncFunction().then(function (value) {
+    console.log(value);
+  });
+  //   let value = await adnanAsyncFunction();
+  console.log("Hi there 1");
+  console.log(value);
+}
+
+main();
