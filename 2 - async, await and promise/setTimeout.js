45b2b8db431394065961beb1b9335c6d9b6e49bb
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-25 23:16:54 +0600

    aync understand

---
+++ 2 - async, await and promise/setTimeout.js
@@ -0,0 +1,23 @@
+function findSum(n) {
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+}
+
+function findSumTill100() {
+  console.log(findSum(100));
+}
+
+// busy waiting
+function syncSleep() {
+  let a = 1;
+  for (let i; i < 10000; i++) {
+    a++;
+  }
+}
+
+syncSleep();
+// setTimeout(findSumTill100, 1000);
+console.log("Hello World");
