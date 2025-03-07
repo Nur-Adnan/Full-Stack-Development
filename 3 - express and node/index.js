c5c68629e52be9a1ea44ada8a2ef30d0f17f106a
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-03-08 03:45:35 +0600

    query params also done

--- 3 - express and node/index.js
+++ 3 - express and node/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json({}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
@@ -13,6 +13,23 @@ app.get("/route-handler", (req, res) => {
   res.json({
     name: "Nur Adnan",
     age: 25,
+  });
+});
+
+app.post("/route-handler/conversation", (req, res) => {
+  const message = req.body.message;
+  console.log(message);
+
+  res.json({
+    output: "2 + 2 = 4",
+  });
+});
+
+app.post("/route-handler/query", (req, res) => {
+  const message = req.query.message;
+  console.log(message);
+  res.json({
+    name: "Adnan",
   });
 });
 
