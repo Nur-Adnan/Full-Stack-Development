15d39d240061a667128f32adbfd4fc7af8b74031
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-03-09 19:46:46 +0600

    middleware concepts clear

--- 6 - middleware and global catches/index.js
+++ 6 - middleware and global catches/index.js
@@ -2,6 +2,41 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//using middleware
+function authMiddleware(req, res, next) {
+  if (username != "adnan" && password != "pass") {
+    res.status(403).send({
+      msg: "Enter a valid user credentials",
+    });
+  } else {
+    next();
+  }
+}
+
+function validKidneyId(req, res, next) {
+  if (kidneyId != 1 && kidneyId != 2) {
+    res.status(403).send({
+      msg: "Invalid Kidney ID",
+    });
+  } else {
+    next();
+  }
+}
+
+// using middleware api
+
+app.get(
+  "/health-checkup-middleware",
+  authMiddleware,
+  validKidneyId,
+  (req, res) => {
+    res.send({
+      msg: "You heart is healthy",
+    });
+  }
+);
+
+// without middleware
 app.get("/health-checkup", (req, res) => {
   const username = req.headers.username;
   const password = req.headers.password;
